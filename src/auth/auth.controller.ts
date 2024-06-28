764f83871bf3cfe02e041ab2e95b4e1e91d8e813
sachin supunthaka <sachinsupunthaka96@gmail.com>
2024-06-28 15:05:08 +0530

    chor: define services and controllers

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signin')
+  async signin() {}
+
+  @Post('signup')
+  async signup() {}
+}
