17d5f84fcbc0be9d0518fe74f0df295cb20e1cb8
sachin supunthaka <sachinsupunthaka96@gmail.com>
2024-07-10 15:04:18 +0530

    chor: prisma migration and controller update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signin')
-  async signin() {
+  async signin(@Body() dto: AuthDto) {
+    console.log(dto);
     return this.authService.signin();
   }
 
   @Post('signup')
-  async signup() {
-    return this.authService.signup();
+  async signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return {
-      msg: 'user signed up',
-    };
+  async signup(dto: AuthDto) {
+    // generate password hash
+
+    const hash = await argon2.hash(dto.password);
+    // save new user in db
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      // return the saved user
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'p2002') {
+          ('Credentials already Taken');
+        }
+      }
+      throw err;
+    }
   }
 
   signin() {
