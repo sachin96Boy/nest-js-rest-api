aa725aa0b25aca504948e99ea5c590feb293c417
sachin supunthaka <sachinsupunthaka96@gmail.com>
2024-07-18 14:27:00 +0530

    chor: http status data on controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -12,6 +12,7 @@ export class AuthController {
     return this.authService.signin(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signup')
   async signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
